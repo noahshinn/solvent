23589d2ee644eba5b142a54ae35a083c6a1f265c
Noah Shinn <noahshinn024@gmail.com>
2022-11-17 01:33:05 -0500

    init nac trainer

--- solvent/models/__init__.py
+++ solvent/models/__init__.py
@@ -1,2 +1,3 @@
 from .model import Model # temp
 from .bin_model import BinModel
+from .nac_model import NACModel
---
+++ solvent/train/nac_trainer.py
@@ -0,0 +1,148 @@
+"""
+STATUS: NOT TESTED
+
+"""
+
+import time
+import torch
+from torch.optim import Adam, SGD
+from torch.optim.lr_scheduler import ExponentialLR, ReduceLROnPlateau
+
+from solvent.train import Trainer
+from solvent.data import DataLoader
+from solvent.nn import NACLoss
+from solvent.logger import NACLogger
+from solvent.utils import to_bin
+from solvent.types import BinPredMetrics
+
+from typing import Union, Dict
+from torch_geometric.data.data import Data
+
+
+class NACTrainer(Trainer):
+    def __init__(
+            self,
+            root: str,
+            run_name: str,
+            model: torch.nn.Module,
+            train_loader: Union[DataLoader, str],
+            test_loader: Union[DataLoader, str],
+            optim: Union[Adam, SGD, None] = None,
+            scheduler: Union[ExponentialLR, ReduceLROnPlateau, None] = None,
+            start_epoch: int = 0,
+            start_lr: float = 0.01,
+            chkpt_freq: int = 1,
+            description: str = ''
+        ) -> None:
+        super().__init__(root, run_name, model, train_loader, test_loader, optim, scheduler, start_epoch, start_lr, chkpt_freq, description)
+        self._loss = NACLoss(self._device)
+        self._logger = NACLogger(self._log_dir, self._is_resume)
+    
+    # TODO: implement
+    def log_metrics(
+            self,
+            nac_mae: torch.Tensor,
+            nac_mse: torch.Tensor
+        ) -> None:
+        self._logger.log_epoch(
+            epoch=self._epoch,
+            lr=self._lr,
+            nac_mae=nac_mae,
+            nac_mse=nac_mse,
+            duration=time.perf_counter() - self._walltime
+        )
+
+    def pred(self, structure: Union[Dict, Data]) -> torch.Tensor:
+        """
+        Evaluates the model.
+
+        N: Number of atoms in the system.
+        M: Number of unique chemical species types
+        K: Number of electronic states.
+
+        Args:
+            structure (Union[Dict, Data]): An atomic system represented as either
+                a Python dictionary or torch-geometric Data object with the following
+                data fields:
+                    `x`: one-hot vector of size (M)
+                    `pos`: coordinates of size (N, 3)
+                    `energies`: energy vector of size (K)
+                    `forces`: force vector of size (K, N, 3)
+
+        Returns:
+            (torch.Tensor): A binary label.
+
+        """
+        out = self._model(structure)
+        return to_bin(out)
+
+    def evaluate(self, loader: DataLoader, mode: str) -> BinPredMetrics:
+        """
+        Full pass through a data set.
+
+        Args:
+            loader (DataLoader): An iterable for a series of structures.
+            mode (str): One of 'TRAIN' or 'TEST'
+
+        Returns:
+            e_mae (torch.Tensor), f_mae (torch.Tensor): Energy force mean absolute
+                error.
+
+        Asserts:
+            - `mode` is one of 'TRAIN' or 'TEST'
+
+        """
+        assert mode == 'TRAIN' or mode == 'TEST'
+        for structure in loader:
+            structure.to(self._device)
+            label = self.pred(structure)
+            self._loss(
+                label,
+                structure['is_like_zero'].to(self._device)
+            )
+            if mode == 'TRAIN':
+                self.step(loss=self._loss.compute_loss())
+        acc, prec, rec, f1 = self._loss.compute_metrics()
+        return BinPredMetrics(acc, prec, rec, f1)
+
+    def update(self, loss: torch.Tensor) -> None:
+        self._walltime = time.perf_counter()
+        if isinstance(self._scheduler, ReduceLROnPlateau):
+            self._scheduler.step(metrics=loss)
+        else:
+            self._scheduler.step()
+        self._epoch += 1
+        self._cur_chkpt_count += 1
+        self._lr = self._optim.param_groups[0]['lr']
+
+    # TODO: figure out type errors
+    def fit(self) -> str:
+        """
+        Args:
+            None
+
+        Returns:
+            (str): exit code
+
+        """
+        while not self.should_terminate():
+            acc_train, prec_train, rec_train, f1_train = self.evaluate(loader=self._train_loader, mode='TRAIN') # type: ignore
+            acc_test, prec_test, rec_test, f1_test = self.evaluate(loader=self._test_loader, mode='TEST') # type: ignore
+
+            self.log_metrics(
+                accuracy_train=acc_train,
+                accuracy_test=acc_test,
+                precision_train=prec_train,
+                precision_test=prec_test,
+                recall_train=rec_train,
+                recall_test=rec_test,
+                f1_train=f1_train,
+                f1_test=f1_test
+            )
+
+            self.update(self._loss.compute_loss())
+
+            if self._cur_chkpt_count == self._chkpt_freq:
+                self.chkpt()
+
+        return self._exit_code
