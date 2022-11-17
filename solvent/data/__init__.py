af2d5ac22f477072188c2354e962faa989aaa4ff
Noah Shinn <noahshinn024@gmail.com>
2022-11-17 17:57:54 -0500

    nac dataset

--- solvent/data/__init__.py
+++ solvent/data/__init__.py
@@ -1,4 +1,4 @@
 from ._dataloader import DataLoader
 from ._dataset import Dataset 
 from .ef_dataset import EnergyForceDataset
-from .nac_dataset import NACBinDataset
+from .bin_dataset import BinDataset
